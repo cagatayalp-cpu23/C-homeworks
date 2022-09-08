7306f7d99b36a0c62ebc03db62956c89ddf66391
cagatayalp-cpu23 <71559331+cagatayalp-cpu23@users.noreply.github.com>
2022-09-08 09:01:04 +0300

    add skip list

--- skiplist.c
+++ skiplist.c
@@ -1,265 +1,291 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-typedef struct list
+typedef struct node
 {
-	int value;
-	struct list *next;
-	struct list *down;
+	struct node *next;
+	struct node *down;
+	int data;
 	
-}LIST;
-LIST **Print(int seviye,LIST**);
-void olustur(LIST *head,int N,int *seviye,LIST **array,int ilk);
-void Search(LIST **head,int seviye);
-void insert(LIST **head,int seviye);
+	
+}NODE;
+void print(NODE **address,int k);
+int add(NODE **address);
+void delete(NODE **address);
+void search(NODE **address,int k);
+
+void create(NODE **address,int length);
+int createsub(NODE **address,int k);
+
 
 int main(int argc, char *argv[]) 
 {
-	
+	NODE *address[100];
 	srand(time(NULL));
-	int N,i,value,seviye=0,ilk;
-	LIST *head=NULL;
-    LIST **array;
-	LIST **head2;
-	printf("how many elements will you create a list");
-	scanf("%d",&N);
+	int length;
 		
-		array=(LIST**)malloc(N*sizeof(LIST*));
-		ilk=N;
-    olustur(head,N,&seviye,array,ilk);
+	printf("how many number do you want to add?");
+	scanf("%d",&length);
+	create(address,length);
+	int k=0,t=1,choice;
+	k=createsub(address,k);
+   
+	print(address,k);
 	
-    
-	head2=Print(seviye,array);
-	
-	
-	LIST **tmp=(LIST**)malloc(seviye*sizeof(LIST*));
-	
-	seviye--;
-	for(i=0;i<seviye;i++)
+	while(t)
 	{
-		tmp[i]=array[i];
+		printf("press 1 to add value, press 2 to delete value press 3 to search value press 4 to exit");
+		scanf("%d",&choice);
+		if(choice==1)
+		{
+			int control=add(address);
+		    if(control==1)
+		    {
+		    	k=createsub(address,0);
+            	
+			}
+			print(address,k);
+			
+		}
+		if(choice==2)
+		{
+			delete(address);
 		
-		
-		
-		
+			k=createsub(address,0);
+        	print(address,k);
+		}
+		if(choice==3)
+		{
+			search(address,k);
+		}
+		if(choice==4)
+		{
+			t=0;
+		}
 	}
 	
-	
-	
-	
-	
-
-	
-		Search(head2,seviye);
-	
-    
-    
-    
-	
-	
-	
-	
-
-
 	return 0;
 }
-void olustur(LIST *head,int N,int *seviye,LIST **array,int ilk)
+void search(NODE **address,int k)
 {
-	
-    int value,i,number,k=0,count=0,gecici;
-	LIST *prev=NULL,*prev2=NULL,*ilkadres;
-	LIST *temp;
-	prev2=NULL;
-	gecici=count;//
-	for(i=0;i<N;i++)
+	int value;
+	printf("enter the value to be searched:");
+	scanf("%d",&value);
+	NODE *temp=address[k];
+	int t=0;
+	while(t==0)
 	{
-	//	array=(LIST**)(realloc(array,(*seviye)+2));
-		number=rand()%100;
-		if(N==ilk)
-		{
-		temp=NULL;
-	    temp=(LIST*)(malloc(sizeof(LIST)));
-	    if(i==0)
-			{
-				array[(*seviye)]=temp;
-				(*seviye)++;
-				
-				
-			}
-	    
-		printf("enter the value:");
-		scanf("%d",&value);
-       temp->value=value;
-       temp->next=NULL;
-       temp->down=NULL;
-       if(prev!=NULL)
-       {
-        	prev->next=temp;
-	   }
-          prev=temp;
-		}
 		
-		if(N-1>gecici){
-	   if(number%2==0)
-	   {
-	   	    LIST *tmp=NULL;
-	   	    tmp=(LIST*)(malloc(sizeof(LIST)));
-	   	    if(N==ilk)
-	   	    {
-	   	    	tmp->value=value;
-	   	    tmp->down=temp;
-	   	    	
-	   	    	
-			   }
-			   else
-			   {
-			   	tmp->value=head->value;
-			   	tmp->down=head;
-			   	
-			   }
-	   	    
-	   	    tmp->next=NULL;
-	   	    if(prev2!=NULL)
-	   	    {
-	   	    	prev2->next=tmp;
-		    }
-		    prev2=tmp;
-		    if(k==0)
-		    {
-		    	k++;
-		    	ilkadres=tmp;
-		    	array[*seviye]=ilkadres;
-		    	(*seviye)++;
-			}
-		    
-	   	     
-	   	    gecici++;
-	   	    
-	     
-	   }
-	   if(ilk!=N&&head!=NULL)
-	   {
-	   	head=head->next;
-	   }
-	  
-	   
-	     
-	   
-	   
-	}}
-//	if(gecici==1)
-//	{
-//		gecici=0;
-//	}
-	 if(gecici==count)
-	   {
-	   	    array[*seviye]=ilkadres;
-		    	(*seviye)++;
-		    	return;
-	   }
-	   else
-	   {
-	   	
-	   	olustur(ilkadres,gecici,seviye,array,ilk);
-	   }
-	
-
-    	
-}
-
-
-
-void insert(LIST **head,int seviye)
-{
-	    LIST **tmp,*deger2;
-    	tmp=head;
-    	int i=seviye-1,value;
-    	printf("\nenter the value to be added:");
-    	scanf("%d",&value);
-    	
-    while(i!=-1)
-    {
-    
-			while(tmp[i]->next!=NULL && tmp[i]->value<value)
+		if(temp==NULL)
 		{
-			tmp[i]=tmp[i]->next;
+			printf("not found");
+			t=1;
 		}
-         if(i==1)
+		else if(value>temp->data)
+    	{
+		if(temp->next==NULL||temp->next->data>value)
 		{
-			deger2=(LIST*)malloc(sizeof(LIST));
-			deger2->next=tmp[i]->next;
-            tmp[i]->next=deger2;
-			i=-1;
-			return;
+		temp=temp->down;
+		k--;
+	//	temp=address[k];	
 		}
-
-		tmp[i]=tmp[i]->down;	
-		i--;	
-	
-	}
-}
-	
-	
-void Search(LIST **head,int seviye)
-{
-     	LIST **tmp;
-    	tmp=head;
-    	int i=seviye-1;
-    	int value;
-    	printf("enter the value to search for :");
-    	scanf("%d",&value);
-    	
-    while(i!=-1)
-    {
-    	
-    	while(tmp[i]->next!=NULL && tmp[i]->value<value)
+		else if(value==temp->next->data)
 		{
-			tmp[i]=tmp[i]->next;
-		}
-		if(tmp[i]->value==value)
-		{
-			printf("finded level: %d\n",i+1);
-			printf("found!");
-			i=-1;
-			return;
-		}
-		else if(i==-1)
-		{
-			printf("not found!");
-			i=-1;
-			return;
+			printf("\nfound in level %d\n",k);
+			t=1;
 		}
 		else
 		{
-		tmp[i]=tmp[i]->down;	
-		i--;	
-		}	
-	}
-				
-		
-}
-
-LIST **Print(int seviye,LIST **array)
-{
-    
-	int i=0;
-	LIST **tmp=(LIST**)malloc(seviye*sizeof(LIST*));
-	
-	seviye--;
-	for(i=0;i<seviye;i++)
-	{
-		tmp[i]=array[i];
-		while(array[i]!=NULL)
-		{
-			printf("%d  ",array[i]->value);
-	    	array[i]=array[i]->next;
+			temp=temp->next;
 		}
 		
-		printf("\n");
+    	}
+    	else if(value<temp->data)
+    	{
 		
+		k--;
+		temp=address[k];	
+		
+    	}
+    	else
+    	{
+    		printf("\nfound in level %d\n",k);
+			t=1;
+		}
 	}
-	return tmp;
 	
 	
 }
+void delete(NODE **address)
+{
+	int value;
+	NODE *head=address[0];
+	NODE *prev;
+	printf("enter the value to be deleted");
+	scanf("%d",&value);
+	while(head!=NULL&&head->data<value)
+	{
+		prev=head;
+		head=head->next;
+	}
+	if(head->data!=value)
+	{
+		printf("not found");
+	}
+	else
+	{
+		prev->next=head->next;
+		free(head);
+	}
+}
+int add(NODE **address)
+{
+	
+	int value;
+	NODE *node=(NODE*)malloc(sizeof(NODE));
+	NODE *head=address[0];
+	NODE *temp=address[0];
+	printf("enter the value:(sorted)");
+	scanf("%d",&value);
+	while(temp!=NULL&&temp->data!=value)
+	{
+		temp=temp->next;
+	}
+	
+	
+	node->data=value;
+	
+	NODE *prev;
+if(temp==NULL)
+	{
+		
+	
+	while(head!=NULL&&head->data<value)
+	{
+		prev=head;
+		head=head->next;
+	}
+	if(head==NULL)
+	{
+		prev->next=node;
+		node->next=NULL;
+	}
+	else if(head==address[0])
+	{
+		address[0]=node;
+		node->next=head;
+	}
+	else
+	{
+		prev->next=node;
+		node->next=head;
+	}
+	return 1;
+   }
+else{
+   	printf("you cant add the same value");
+   	return 0;
+   }
+}
+void print(NODE **address,int k)
+{
+	printf("\n\n");
+	while(k!=-1)
+	{
+		NODE *head=address[k];
+	    
+		while(head!=NULL)
+		{
+			printf("%d  ",head->data);
+			head=head->next;
+		}
+		k--;
+		printf("\n");
+	
+		
+		
+	}
+}
+void create(NODE **address,int length){
+	
+	int i=0;
+	NODE *prev;
+	printf("\nenter the numbers:\n");//?
+	for(i=0;i<length;i++)
+	{
+		NODE *node=(NODE*)malloc(sizeof(NODE));
+		scanf("%d",&(node->data));
+		node->next=NULL;
+		node->down=NULL;
+		if(i==0)
+		{
+		
+			address[0]=node;
+			
+		}
+		else
+		{
+			prev->next=node;
+		}
+		prev=node;
+		
+	}
+	
+	
+	
+	
+	
+}
+int createsub(NODE **address,int k)
+{
+//	HEED *temp=head;
+    NODE *head=address[k];
+	NODE *prev;
+
+	int i=0;
+        while(head!=NULL)
+		{
+			 
+
+        	if(rand()%2==0)
+        	{
+        		
+        		NODE *node=(NODE*)malloc(sizeof(NODE));
+        		node->data=head->data;
+        		node->down=head;
+        		node->next=NULL;
+        		if(i==0)
+        		{
+        			k++;
+        			address[k]=node;
+					
+					
+				}
+				else
+				{
+					prev->next=node;
+				}
+				prev=node;
+				i++;
+            	
+
+			}
+			
+			    
+				head=head->next;
+			
+			
+			
+  
+        	
+		}
+		if(i==0||i==1)
+		{
+			
+			return k;
+		}
+		createsub(address,k);
+}
+
